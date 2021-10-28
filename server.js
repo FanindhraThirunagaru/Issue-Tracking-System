5d04181a9a0f97fec100d823a8ef697618aad07a
FanindhraThirunagaru <46750856+FanindhraThirunagaru@users.noreply.github.com>
2021-10-28 23:22:27 +0530

    integration with mysql localhost server

--- server.js
+++ server.js
@@ -1,31 +1,88 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const https = require('https');
+const mysql = require('mysql');
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/",function(req,res){
-  res.sendFile(__dirname+"/index.html");
-});
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/login", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/login.html");
 });
 
-app.post("/login", function(req, res) {
+app.post("/", function(req, res) {
+
+  const connection = mysql.createConnection({
+  	host: "localhost",
+  	user: "testroot",
+  	password: "Fanny@&161808",
+    database: "project"
+  });
+
   var userName = req.body.username;
   var password = req.body.password;
-  res.send(userName + " " + password);
+  connection.connect(function(err){
+    if(err) throw err;
+		var sql = "select userPassword from users where userName = '"+userName+"';";
+		connection.query(sql,function(err,result){
+			if(err) throw err;
+      if(result === {}){
+        res.send("User Not Found!!!");
+      }else if (result[0].userPassword === password) {
+        res.send("Login successful");
+      }else{
+        res.send("password incorrect");
+      }
+		});
+  });
 });
 
-app.get("/createAccount",function(req,res){
-  res.sendFile(__dirname+"/createAccount.html");
+app.get("/register", function(req, res) {
+  res.sendFile(__dirname + "/createAccount.html");
 });
 
-app.post("/createAccount", function(req,res){
+app.post("/register", function(req, res) {
   var firstName = req.body.firstname;
   var middleName = req.body.middlename;
   var lastName = req.body.lastname;
-  res.send(firstName+" "+middleName+" "+lastName);
+  var mailID = req.body.email;
+  var password = req.body.password;
+  var userName = req.body.userName;
+
+  const connection = mysql.createConnection({
+  	host: "localhost",
+  	user: "testroot",
+  	password: "Fanny@&161808",
+    database: "project"
+  });
+
+  const employeeValues = [[firstName,middleName,lastName,mailID]];
+
+  connection.connect(function(err){
+    var sql = "insert into employees (firstName,middleName,lastName,mailID) values ?;";
+    connection.query(sql,[employeeValues],function(err, result){
+      if(err) throw err;
+      else{
+        res.write("Employee created successfully");
+      }
+    });
+    connection.query("select employeeID from employees where mailID = '"+mailID+"';",function(err, result){
+      if(err) throw err;
+      else{
+        const userValues = [[userName,password,result[0].employeeID]];
+        connection.query("insert into users values ?;",[userValues],function(err, result){
+          res.end("/nsuccessful");
+        });
+      }
+    });
+  });
+
+});
+
+app.get("/addProjects", function(req, res) {
+  res.sendFile(__dirname + "/addProjects.html");
 });
 
 app.listen(8080, function() {
