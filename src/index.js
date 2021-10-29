f52099c1f1639855cf11345dfb222de5b338cd8e
SaiSudhamsa <45008003+SaiSudhamsa@users.noreply.github.com>
2021-10-29 06:56:28 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import Sidebar from "./Components/Sidebar";
+import Topbar from "./Components/Topbar";
+import "./index.css";
+//statesless functional component
+//always return JSX
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Greeting(){//capitalizing is important so react knows its a component
+  return (
+    <div className="Greeting">
+      <Topbar />
+      <Sidebar />
+    </div>
+    ); //this is called jsx.
+}
+// const Greeting = () => {
+//   return React.createElement(
+//     'div', 
+//     {},
+//     React.createElement('h1',{}, 'helloworld')
+//   );
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<Greeting/>, document.getElementById('root'));
+
+/* 
+JSX rules:
+return single element
+camelCase for htlm attributes
+clasName instead of class
+close every element, even ones which are excused in html.
+good idea to use parenthesis for return.
+*/
+
+/* arrow functions
+implicit and explicit return */
